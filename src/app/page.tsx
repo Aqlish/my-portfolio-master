0c88fed3e896a125c99f19fb962c50d223ac2dd5
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-11-10 16:49:26 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,19 @@
-"use client"
-
+"use client";
 
 import Hero from "@/Components/Hero";
-// import Image from "next/image";
 import Project from "@/Components/Project";
 import Contact from "@/Components/Contact";
 import About from "@/Components/About";
-import Aos from"aos"
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 import Skills from "@/Components/Skills";
 
-
-
-
 export default function Home() {
-  useEffect (() =>{
-    Aos.init({
-      easing:"ease-out-back",
-      duration:1200,
-      delay:100,
-      mirror:true,
-      anchorPlacement:"bottom-bottom",
-      offset:160,
-    })
-    Aos.refresh()
-  },[]);
   return (
-   <main>
-    <Hero />
-    <Project />
-    <Contact />
-    <Skills />
-    <About/>
-    
-   </main>
+    <main>
+      <Hero />
+      <Project />
+      <Contact />
+      <Skills />
+      <About />
+    </main>
   );
 }
